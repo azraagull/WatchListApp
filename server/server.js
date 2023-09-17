5e9db2138c75d6f2ffad231711fa66b36b5f66b5
ecemkmz <ecemk259@gmail.com>
2023-09-17 14:57:12 +0300

    17.09.2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const mongoose = require("mongoose");
+const express = require("express");
 const dotenv = require("dotenv");
 
 const app = express();
@@ -8,12 +8,15 @@ const port = process.env.PORT || 5500;
 
 //routes
 const actorRoute = require("./src/api/routes/actors.js");
+const movieRoute = require("./src/api/routes/movies.js");
+const categoryRoute = require("./src/api/routes/categories.js");
 
 dotenv.config();
-
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://azra:v2oYtxHemMQ4MHTM@cluster0.r8ydkmh.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("Connected to mongoDB");
   } catch (error) {
     throw error;
@@ -24,49 +27,9 @@ const connect = async () => {
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/routes/actors", actorRoute);
-
-// const ActorSchema = new mongoose.Schema({
-//   nconst: String,
-//   primaryName: String,
-//   birthYear: String,
-//   deathYear: String,
-//   primaryProfession: String,
-//   knownForTitles: String,
-// });
-
-// const Actors = mongoose.model("Actors", ActorSchema);
-// app.use(express.json());
-// const apiKey = "e7c680bb91msh7cefc06feb84bf0p16346fjsn68ee6f3b768b";
-// const host = "moviesdatabase.p.rapidapi.com";
-// const limitPerPage = 50;
-// const totalPages = 50;
-// for (let page = 1; page <= totalPages; page++) {
-//   app.get(`/actors/${page}`, async (req, res) => {
-//     const options = {
-//       method: "GET",
-//       url: "https://moviesdatabase.p.rapidapi.com/actors",
-//       params: {
-//         page: page,
-//         limit: limitPerPage,
-//       },
-//       headers: {
-//         "X-RapidAPI-Key": apiKey,
-//         "X-RapidAPI-Host": host,
-//       },
-//     };
-
-//     try {
-//       const response = await axios.request(options);
-//       const actors = response.data.results;
-//       await Actors.insertMany(actors);
-//       res.json(response.data);
-//     } catch (error) {
-//       console.error("Hata:", error);
-//       return res.status(500).json({ error: "Veri çekme hatası" });
-//     }
-//   });
-// }
+app.use("/api", actorRoute);
+app.use("/api", movieRoute);
+app.use("/api", categoryRoute);
 
 app.listen(port, () => {
   connect();
--- server/src/api/routes/actors.js
+++ server/src/api/routes/actors.js
@@ -1,15 +1,16 @@
-const Actor = require("../../models/Actor.js");
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const Actor = require("../../models/Actor.js");
 
 const apiKey = "e7c680bb91msh7cefc06feb84bf0p16346fjsn68ee6f3b768b";
 const host = "moviesdatabase.p.rapidapi.com";
 const limitPerPage = 50;
 const totalPages = 50;
 
-for (let page = 1; page <= totalPages; page++) {
-  router.get(`/actors/${page}`, async (req, res) => {
+router.get("/actors", async (req, res) => {
+  let allActors = [];
+  for (let page = 1; page <= totalPages; page++) {
     const options = {
       method: "GET",
       url: "https://moviesdatabase.p.rapidapi.com/actors",
@@ -26,13 +27,41 @@ for (let page = 1; page <= totalPages; page++) {
     try {
       const response = await axios.request(options);
       const actors = response.data.results;
-      await Actor.insertMany(actors);
-      res.json(response.data);
+      allActors = allActors.concat(actors);
     } catch (error) {
       console.error("Hata:", error);
       return res.status(500).json({ error: "Veri çekme hatası" });
     }
-  });
-}
+  }
+  res.json(allActors);
+  await Actor.insertMany(allActors);
+});
+
+router.get("/actors/:id", async (req, res) => {
+  const actorId = req.params.id;
+
+  const options = {
+    method: "GET",
+    url: `https://moviesdatabase.p.rapidapi.com/actors/${actorId}`,
+    headers: {
+      "X-RapidAPI-Key": "e7c680bb91msh7cefc06feb84bf0p16346fjsn68ee6f3b768b",
+      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    const actorData = response.data;
+
+    if (!actorData) {
+      return res.status(404).json({ error: "Aktör bulunamadı" });
+    }
+
+    res.json(actorData);
+  } catch (error) {
+    console.error("Hata:", error);
+    return res.status(500).json({ error: "Veri çekme hatası" });
+  }
+});
 
 module.exports = router;
