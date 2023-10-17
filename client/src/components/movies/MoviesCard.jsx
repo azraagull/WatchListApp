2dfb0ed1e7308e7b455242a32b8943ab711c1def
azraagull <azragul85@gmail.com>
2023-10-17 21:55:03 +0300

    17.10.3

--- client/src/components/movies/MoviesCard.jsx
+++ client/src/components/movies/MoviesCard.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Card } from "antd";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Card, Pagination } from 'antd';
+const apiURL = 'http://localhost:5500/api/movies';
 const { Meta } = Card;
-const apiURL = "http://localhost:5500/api/movies";
+
 
 const MoviesCard = () => {
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const itemsPerPage = 50;
 
   useEffect(() => {
-    axios
-      .get(apiURL)
-      .then((response) => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiURL, {
+          params: {
+            page: currentPage,
+            limit: itemsPerPage,
+          },
+        });
+
         setMovies(response.data.docs);
-      })
-      .catch((error) => {
-        console.error("Veri çekme hatası:", error);
-      });
-  }, []);
+        setTotalPages(response.data.totalPages);
+      } catch (error) {
+        console.error('Veri çekme hatası:', error);
+      }
+    };
+
+    fetchData();
+  }, [currentPage]);
 
   const cardStyle = {
     width: "200px",
@@ -28,7 +41,7 @@ const MoviesCard = () => {
     objectFit: "cover",
     width: "100%",
     height: "100%",
-  };
+  }
 
   return (
     <div className="container mx-auto mt-4 mb-4">
@@ -43,6 +56,15 @@ const MoviesCard = () => {
             </Card>
           </div>
         ))}
+      </div>
+
+      <div className="pagination-container items-center justify-between">
+        <Pagination
+          current={currentPage}
+          total={totalPages * itemsPerPage}
+          pageSize={itemsPerPage}
+          onChange={(page) => setCurrentPage(page)}
+        />
       </div>
     </div>
   );
