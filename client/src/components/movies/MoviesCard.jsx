423d3d9c70bbd4212f95854c038ea89cd2d80110
azraagull <azragul85@gmail.com>
2023-10-17 16:16:53 +0300

    17.10.2

--- client/src/components/movies/MoviesCard.jsx
+++ client/src/components/movies/MoviesCard.jsx
@@ -1,30 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, Pagination } from 'antd';
-
+import { Card} from 'antd';
+const apiURL = 'http://localhost:5500/api/movies'
 const { Meta } = Card;
 
-const ActorList = () => {
-  const [actors, setActors] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 12; // Her sayfada gösterilecek aktör sayısı
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     // Sunucu tarafındaki API rotasını çağıralım
-    axios.get('/api/actors') // Sunucu tarafında belirttiğiniz API rotasına göre güncelleyin
+    axios.get(apiURL) // Sunucu tarafında belirttiğiniz API rotasına göre güncelleyin
       .then((response) => {
         // API'den gelen aktör verilerini state'e kaydedelim
-        setActors(response.data);
+        setMovies(response.data.docs);
       })
       .catch((error) => {
         console.error('Veri çekme hatası:', error);
       });
   }, []);
 
-  const currentActors = actors.slice(
-    (currentPage - 1) * pageSize,
-    currentPage * pageSize
-  );
 
   const cardStyle = {
     width: '200px', // Genişlik
@@ -34,27 +28,23 @@ const ActorList = () => {
   return (
     <div className="container mx-auto mt-4 mb-4">
       <div className="flex flex-wrap">
-        {currentActors.map((actor) => (
-          <div key={actor._id} className="p-5">
-            <Card
-              hoverable
-              style={cardStyle}
-            >
-              <Meta title={actor.primaryName} description={`Profession: ${actor.primaryProfession}`} />
-            </Card>
-          </div>
+        {movies.map(movie => (
+          <div key={movie._id} className="p-5">
+          <Card
+            hoverable
+            style={cardStyle}
+            cover={<img alt={movie.name} src={movie.poster} />}
+          >
+            <Meta
+              title={movie.name}
+            />
+          </Card>
+        </div>
         ))}
       </div>
-      <div className="mt-4 flex justify-center">
-        <Pagination
-          current={currentPage}
-          total={actors.length}
-          pageSize={pageSize}
-          onChange={(page) => setCurrentPage(page)}
-        />
-      </div>
+      
     </div>
   );
 };
 
-export default ActorList;
+export default MovieList;
--- client/src/pages/moviesPage.jsx
+++ client/src/pages/moviesPage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import MoviesCard from "../components/movies/MoviesCard";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import ActorList from "../components/movies/MoviesCard";
+import MovieList from "../components/movies/MoviesCard";
 
 const moviesPage = () => {
   return (
     <div>
       <Header />
-      <ActorList />
+      <MovieList />
       <Footer />
     </div>
   );
